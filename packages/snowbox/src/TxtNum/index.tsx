6123f566f3bca192ffce5c43158e1e361da784cd
hanweijing <hanweijing@xueqiu.com>
2022-09-28 14:45:32 +0800

    fix: 组件文档补充

--- packages/snowbox/src/TxtNum/index.tsx
+++ packages/snowbox/src/TxtNum/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleProp, Text } from 'react-native';
-import { Txt } from 'snowbox';
+import { Txt } from '../';
 import { fontTypes } from '../Utils/props';
 
 type Props = {
